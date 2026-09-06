6a46bffe7bb56eaf26edf2cb41dc7dcf2533b5ff
Jokubas <jokubasjokna@gmail.com>
2026-09-07 00:34:10 +0300

    Add health endpoint

---
+++ src/main/java/com/jokubas/aicodereviewer/controller/HealthController.java
@@ -0,0 +1,16 @@
+package com.jokubas.aicodereviewer.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public String health() {
+        return "OK";
+    }
+
+
+}
